c64092d6ab1e86cfb3b8b5ee64923ed1b83bb0ff
Syed Moiz Ul Hassan <syedmoizulhassanshah@gmail.com>
2022-06-21 22:54:50 +0500

    added initializable check in contracts

--- scripts/upgrade_UUPS.js
+++ scripts/upgrade_UUPS.js
@@ -1,6 +1,6 @@
 // scripts/prepare_upgrade.js
 async function main() {
-    const proxyAddress = '0x1397cbC428E0Bfc599eFB31640F909C12144CDB5';
+    const proxyAddress = '0x5d021454D9Fb9442fE321d2FdEb8BBf00199422a';
    
     const DroneContractV2 = await ethers.getContractFactory("DroneContractV2");
     console.log("Preparing upgrade...");
